b5dfd9c78f2973bbe732f2afeccf640da1ea7ea8
bruceg14 <ziyuan.guo@vanderbilt.edu>
2024-12-28 02:12:39 -0700

    Change to search .asset files and complete the blobs collection in 8 hours

--- myScript.sh
+++ myScript.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # echo 'Script to get the first 10 Unity file with .fbx extension from World of Code with their project name as output'
 
-zcat /da?_data/basemaps/gz/b2fFullU*.s | fgrep .fbx | head -n 10 | tee fbx_blobs_10.txt #Search the data file and find all the files with .fbx ending select the top 10 and store them in 'fbx_blobs_10.txt'
-cat fbx_blobs_10.txt |cut -d \; -f 1 | ~/lookup/getValues b2P | tee fbx_projects_10.txt #Go through the fbx_blobs_10.txt and use the first field which is the blobs to search for project names and store then in "fbx_projects_10.txt"
-cat fbx_projects_10.txt | cut  -d";" -f2- >> fbx_project_10_fork.txt #Go through the list of "blob;ProjectsNames" structure and extract the project names that is in a deforked group
-cat fbx_project_10_fork.txt | cut -d ';' -f 1 >> fbx_project_10_single.txt #Select the original project name and keep the original
-sort -u fbx_project_10_single.txt -o fbx_project_10_single_no_dup.txt #Make sure there is no duplicat in the project names
+# #Search the data file and find all the files with .fbx ending select the top 10 and store them in 'fbx_blobs_10.txt'
+# zcat /da?_data/basemaps/gz/b2fFullU*.s | fgrep .fbx | head -n 10 >> fbx_blobs_10.txt 
+# #Go through the fbx_blobs_10.txt and use the first field which is the blobs to search for project names and store then in "fbx_projects_10.txt"
+# cat fbx_blobs_10.txt |cut -d \; -f 1 | ~/lookup/getValues b2P >> fbx_projects_10.txt 
+# #Go through the list of "blob;ProjectsNames" structure and extract the project names that is in a deforked group
+# cat fbx_projects_10.txt | cut  -d";" -f2- >> fbx_project_10_fork.txt 
+# #Select the original project name and keep the original
+# cat fbx_project_10_fork.txt | cut -d ';' -f 1 >> fbx_project_10_single.txt 
+# #Make sure there is no duplicat in the project names
+# sort -u fbx_project_10_single.txt -o fbx_project_10_single_no_dup.txt 
 
 
 # Attemp to store the value of blob to File name into a SQlite database in da4
@@ -17,3 +22,12 @@ sort -u fbx_project_10_single.txt -o fbx_project_10_single_no_dup.txt #Make sure
 #     INSERT INTO fbx_blob_fname (blob_id, filename) VALUES ('$blob', '$safe_filename');
 # EOF
 # done
+
+
+#Change the search to ".asset" files
+zcat /da?_data/basemaps/gz/b2fFullU*.s | grep -iE "Assets/.+\.asset" >> asset_blobs_new.txt
+grep -v "\.asset\.meta$" asset_blobs_new.txt > filtered_blobs.txt
+cat filtered_blobs.txt |cut -d \; -f 1 | ~/lookup/getValues b2P >> asset_projects.txt
+cat asset_projects.txt | cut  -d";" -f2- >> asset_project_defork.txt 
+cat asset_project_defork.txt | cut -d ';' -f 1 >> asset_project_only.txt
+sort -u asset_project_only.txt -o asset_project_only_no_dup.txt
